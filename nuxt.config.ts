6cc98acd88118bde1c709bca390a2647aa119432
RhenCloud <i@rhen.cloud>
2025-12-15 23:51:39 +0800

    fix: 为 Font Awesome 资源添加 font-display=swap 参数以优化加载

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,20 +25,20 @@ export default defineNuxtConfig({
                 {
                     rel: "preload",
                     as: "style",
-                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css?font-display=swap",
                     crossorigin: "anonymous",
                 },
                 {
                     rel: "preload",
                     as: "font",
-                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/webfonts/fa-solid-900.woff2",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/webfonts/fa-solid-900.woff2?font-display=swap",
                     type: "font/woff2",
                     crossorigin: "anonymous",
                 },
                 {
                     rel: "preload",
                     as: "font",
-                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/webfonts/fa-brands-400.woff2",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/webfonts/fa-brands-400.woff2?font-display=swap",
                     type: "font/woff2",
                     crossorigin: "anonymous",
                 },
